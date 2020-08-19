a918970b3b728514bcb50c1493885c26c4cecaa0
Zackary Cammack <zack.cammack@gmail.com>
2020-08-19 13:04:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(integer)
-  while integer > 0
-  puts "#{integer} SECONDS(S)!"
-  integer -= 1
+def countdown(number)
+  while numer > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
   end
   "HAPPY NEW YEAR!"
 end
