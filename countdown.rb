4ff35837f86a11ceb136ac5a519f57a9c0ac1233
Zackary Cammack <zack.cammack@gmail.com>
2020-08-19 13:11:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number)
   while number > 0
   puts "#{number} SECOND(S)!"
+  sleep(2)
   number -= 1
-  sleep 2.seconds
   end
   "HAPPY NEW YEAR!"
 end
